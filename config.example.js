a71f3193bfc5433f343975cba4e36023183196e9
nestattacked <yang283349242@gmail.com>
2017-08-14 00:04:52 +0800

    refresh more files to CDN

---
+++ config.example.js
@@ -0,0 +1,14 @@
+module.exports = {
+  accessKeyId: '',
+  secretAccessKey: '',
+  dirs: [{
+    serverPath: '/usr/share/nginx/html/blog',
+    hostName: 'https://nestattacked.com'
+  }, {
+    serverPath: '/usr/share/nginx/html/app',
+    hostName: 'https://app.nestattacked.com'
+  }, {
+    serverPath: '/usr/share/nginx/html/static/regdict/img',
+    hostName: 'https://static.nestattacked.com/regdict/img'
+  }]
+};
--- index.js
+++ index.js
@@ -1,35 +1,31 @@
 var aliyun = require('aliyun-sdk');
 var recursive = require('recursive-readdir');
-
-var config = {
-    accessKeyId: '',
-    secretAccessKey: '',
-    serverPath: '',
-    hostName: ''
-};
+var config = require('./config.js');
 
 var cdn = new aliyun.CDN({
-    accessKeyId: config.accessKeyId,
-    secretAccessKey: config.secretAccessKey,
-    endpoint: 'https://cdn.aliyuncs.com',
-    apiVersion: '2014-11-11'
+  accessKeyId: config.accessKeyId,
+  secretAccessKey: config.secretAccessKey,
+  endpoint: 'https://cdn.aliyuncs.com',
+  apiVersion: '2014-11-11'
 });
 
 function pushToCDN(url){
-    cdn.pushObjectCache({
-        ObjectPath: url
-    },function(err,res){});
+  cdn.pushObjectCache({
+    ObjectPath: url
+  },function(err,res){});
 }
 
-recursive(config.serverPath,function(err,files){
+config.dirs.forEach(function(dir){
+  recursive(dir.serverPath,function(err,files){
     files.forEach(function(file){
-        //push file to CDN
-        var url = file.replace(config.serverPath, config.hostName);
+      //push file to CDN
+      var url = file.replace(dir.serverPath, dir.hostName);
+      pushToCDN(url);
+      //if url contains index.html, push url without index.html again
+      if(url.indexOf('index.html')!==-1){
+        url = url.replace('index.html','');
         pushToCDN(url);
-        //if url contains index.html, push url without index.html again
-        if(url.indexOf('index.html')!==-1){
-            url = url.replace('index.html','');
-            pushToCDN(url);
-        }
+      }
     });
+  });
 });
